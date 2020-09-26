a69c5507bc21769a64092041b959f48cbfdcaea3
Sam Buters <sambuters@gmail.com>
2020-09-26 16:19:31 -0300

    [#80] Adding TudoGostoso crawling results

--- index.js
+++ index.js
@@ -7,47 +7,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/search', (req, res) => {
-    let crawler = new Crawler('torta');
+    let crawler = new Crawler(req.query.q);
     crawler.getResults().then((results) => {
         res.json(results);
-    });
-});
-
-app.get('/tudogostoso', (req, res) => {
-    let tg_results = [];
-    let tg_crawler = new Crawler({
-        maxConnections: 10,
-        callback: (error, res, done) => {
-            if(error) {
-                console.log(error);
-            } else {
-                var $ = res.$;
-                let recipe_cards = $(".recipe-card");
-                
-                for(var c = 0; c < recipe_cards.length; c += 1) {
-                    let recipe_info = recipe_cards[c].children.pop();
-                    let recipe_picture = recipe_info.children[0];
-                    let recipe_text_info = recipe_info.children[2];
-                    
-                    let recipe_link = recipe_info.attribs.href;
-                    let recipe_title = recipe_text_info.children[3].children[0].data.replace(/\n/g, '');
-                    let recipe_img_url = recipe_picture.children[1].attribs.src;
-
-                    tg_results.push({
-                        link: recipe_link,
-                        title: recipe_title,
-                        img_url: recipe_img_url
-                    });
-                }
-            }
-
-            done();
-        }
-    });
-
-    tg_crawler.queue(`https://www.tudogostoso.com.br/busca?q=${req.query.param}`);
-    tg_crawler.on('drain', () => {
-        res.json(tg_results);
     });
 });
 
--- src/models/Crawler.js
+++ src/models/Crawler.js
@@ -13,7 +13,39 @@ module.exports = class Crawler {
     }
 
     generate_websites_queue() {
-        return;
+        //Tudo Gostoso
+        this.websites_queue.push({
+            uri: `https://www.tudogostoso.com.br/busca?q=${this.search_term}`,
+            callback: (error, res, done) => {
+                if(error) {
+                    console.log(error);
+                } else {
+                    let $ = res.$;
+                    let recipe_cards = $(".recipe-card");
+                    let results = [];
+                    
+                    for(var c = 0; c < recipe_cards.length; c += 1) {
+                        let recipe_info = recipe_cards[c].children.pop();
+                        let recipe_picture = recipe_info.children[0];
+                        let recipe_text_info = recipe_info.children[2];
+                        
+                        let recipe_link = recipe_info.attribs.href;
+                        let recipe_title = recipe_text_info.children[3].children[0].data.replace(/\n/g, '');
+                        let recipe_img_url = recipe_picture.children[1].attribs.src;
+
+                        results.push({
+                            link: recipe_link,
+                            title: recipe_title,
+                            img_url: recipe_img_url
+                        });
+                    }
+
+                    this.search_results.push({name: 'tudogostoso', results: results});
+                }
+
+                done();
+            }
+        });
     }
 
     getResults() {
@@ -22,7 +54,7 @@ module.exports = class Crawler {
         return new Promise((resolve, reject) => {
             this.search_crawler.queue(this.websites_queue);
             this.search_crawler.on('drain', () => {
-                resolve(search_results);
+                resolve(this.search_results);
             });
         });
     }
