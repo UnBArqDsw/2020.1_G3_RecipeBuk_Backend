f0025a99aee325706862b1660938847c69ed6555
Eduardo <eduardolima.df@gmail.com>
2020-09-26 13:58:38 -0300

    [#82] Updating index

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const dbConnect = require("./config/dbConfig");
+
 var express = require('express')
 var app = express()
+
+dbConnect();
 
 app.get('/', function (req, res) {
   res.send('Recipe Buk')
