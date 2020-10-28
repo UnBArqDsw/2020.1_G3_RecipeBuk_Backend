a952f2e08ff119842712aa744bfa18416c5102b5
Sam Buters <sambuters@gmail.com>
2020-10-28 20:11:12 -0300

    [#96] Adding DatabaseSearcher logic

--- src/models/DatabaseSearcher.js
+++ src/models/DatabaseSearcher.js
@@ -9,8 +9,26 @@ module.exports = class DatabaseSearcher extends Searcher {
 
 	getResults() {
 		return new Promise((resolve, reject) => {
-			console.log("Not implemented");
-			resolve([]);
+			this.connection.query("SELECT recipeId, name, time, portions FROM recipe WHERE visibility = TRUE", (err, res) => {
+				if(err) {
+					console.log(err);
+					resolve([]);
+				}
+
+				else {
+					let results = [];
+
+					res.rows.forEach((row) => {
+						results.push({
+							link: `/recipes/${row.recipeid}`,
+							title: row.name,
+							img_url: 'https://www.callinvest.com.br/wp-content/uploads/2017/08/indisponivel.png'
+						});
+					});
+
+					resolve({name: 'recipebuk', results: results});
+				}
+			});
 		});
 	}
 }
--- src/routes/SearchRoutes.js
+++ src/routes/SearchRoutes.js
@@ -3,11 +3,12 @@ const routes = express.Router();
 const CompoundSearcher = require('../models/CompoundSearcher');
 const Crawler = require('../models/Crawler');
 const DatabaseSearcher = require('../models/DatabaseSearcher');
+const db = require('../../db/dbConfig');
 
 routes.get('/search', (req, res) => {
 	let searcher = new CompoundSearcher();
 	searcher.addChild(new Crawler(req.query.q, req.query.page));
-	searcher.addChild(new DatabaseSearcher(null, req.query.q));
+	searcher.addChild(new DatabaseSearcher(db, req.query.q));
 
     searcher.getResults().then((results) => {
         res.json(results[0].length ? results[0] : results[1]);
