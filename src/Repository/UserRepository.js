7710a9d883f17f7af27aea1984377a4ba18417a7
Eduardo Lima <eduardolima.df@gmail.com>
2020-10-28 18:24:12 -0300

    [#68] Adding login and update user

--- src/Repository/UserRepository.js
+++ src/Repository/UserRepository.js
@@ -1,4 +1,5 @@
 const db = require('../../db/dbConfig');
+const User = require('../models/User')
 
 async function addUser(name, email) {
     const query = {
@@ -6,9 +7,9 @@ async function addUser(name, email) {
         values: [name, email],
     }
     var result;
-    try{
-     result = await db.query(query)
-    } catch(err) {
+    try {
+        result = await db.query(query)
+    } catch (err) {
         console.error(err);
         result = err;
     }
@@ -21,33 +22,68 @@ async function deleteUser(email) {
         values: [email],
     }
     var result;
-    try{
+    try {
         result = await db.query(query)
-    } catch(err) {
+    } catch (err) {
         console.error(err)
         result = err;
     }
     return result;
 }
 
-async function login(email, password){
-    let user = new User();
+async function login(email) {
+
     const query = {
         text: "SELECT * FROM USER_ACCOUNT WHERE email = $1",
         values: [email],
     }
-    db.query(query, (err, res) => {
-        if (err) {
-            console.log("Erro: ", err);
-            return err;
-        } else {
-            console.log(res);
-            user.email = res.email;
-            user.name = res.name;
-            //firebase.auth
-        }
-    })
-    return user;
+    try {
+        result = await db.query(query);
+    } catch (err) {
+        console.error(err)
+        return err;
+    }
+    if (result.rowCount > 0) {
+        let user = new User(result.rows[0].email, result.rows[0].name);
+        return user;
+    } else {
+        return "Usuário não encontrado"
+    }
+
+
 }
 
-module.exports = { addUser, deleteUser, login};
+async function updateUser(email,name) {
+
+    const query = {
+        text: "UPDATE USER_ACCOUNT SET name = $1 WHERE email = $2",
+        values: [name,email],
+    }
+    try {
+        result = await db.query(query);
+        console.log(result);
+        const query2 = {
+            text: "SELECT * FROM USER_ACCOUNT WHERE email = $1",
+            values: [email],
+        }
+        try {
+            result = await db.query(query2);
+            console.log(result.rows[0].name)
+        } catch (err) {
+            console.error(err)
+        }
+    } catch (err) {
+        console.error(err)
+        return err;
+    }
+    if (result.rowCount > 0) {
+        let user = new User(email, name);
+        return user;
+    } else {
+        return "Usuário não encontrado"
+    }
+
+
+}
+
+module.exports = { addUser, deleteUser, login, updateUser };
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -7,8 +7,8 @@ var firebase = require("firebase/app");
 
 routes.post('/createUser', async (req, res, next) => {
     var body = req.body;
-    userRepository.addUser(body.name, body.email).then( ret => {
-        if(ret.name == "error"){
+    userRepository.addUser(body.name, body.email).then(ret => {
+        if (ret.name == "error") {
             res.status(500).json({
                 Message: ret.detail
             })
@@ -25,18 +25,18 @@ routes.post('/createUser', async (req, res, next) => {
                 })
             })
         }
-        }).catch(e => {
-            res.status(500).json({
-                Message: e
-            })
+    }).catch(e => {
+        res.status(500).json({
+            Message: e
         })
+    })
 });
 
 routes.post('/deleteUser', async (req, res, next) => {
     var body = req.body;
-    
+
     userRepository.deleteUser(body.email).then(ret => {
-        if(ret.name == "error"){
+        if (ret.name == "error") {
             res.status(500).json({
                 Message: ret.detail
             })
@@ -44,8 +44,59 @@ routes.post('/deleteUser', async (req, res, next) => {
             res.status(200).json({
                 Message: "Usuário removido"
             })
-        }        
+        }
     })
+
+});
+
+routes.post('/login', async (req, res, next) => {
+    var body = req.body;
+
+    userRepository.login(body.email).then(ret => {
+        console.log(typeof(ret))
+        if(typeof(ret) == "string"){
+            res.status(500).json({
+                Message: ret                
+            })
+        } else {
+            firebase.auth().signInWithEmailAndPassword(body.email, body.password).then(() =>{
+                res.json({
+                    Message: ret
+                })
+            }).catch( err =>{
+                res.status(500).json({
+                    Message: err
+                })
+            });
+        }
+
+    }).catch(err=>{
+        res.status(500).json({
+            Message: err
+        })
+    }) 
+
+});
+
+routes.post('/updateUser', async (req, res, next) => {
+    var body = req.body;
+
+    userRepository.updateUser(body.email,body.name).then(ret => {
+        if(typeof(ret) == "string"){
+            res.status(500).json({
+                Message: ret                
+            })
+        } else {
+            res.json({
+                Message: ret
+            })
+        }
+
+    }).catch(err=>{
+        res.status(500).json({
+            Message: err
+        })
+    }) 
 
 });
 
