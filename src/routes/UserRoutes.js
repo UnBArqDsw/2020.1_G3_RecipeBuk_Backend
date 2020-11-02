fbcc5161bb0cb2d8a4c0e397901445bf615e2c5c
Sam Buters <sambuters@gmail.com>
2020-11-02 20:19:32 -0300

    [#140] Moving code to Repository
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -109,62 +109,11 @@ routes.post('/updateUser', async (req, res, next) => {
 });
 
 routes.post('/getFavorites', (req, res) => {
-    let favorites = [];
-
-    db.query(`SELECT FAVORITE.recipeLink
-                FROM USER_SESSION INNER JOIN USER_ACCOUNT ON USER_ACCOUNT.email = USER_SESSION.userEmail
-                INNER JOIN FAVORITE ON FAVORITE.userEmail = USER_ACCOUNT.email
-                WHERE USER_SESSION.sessionId = '${req.body.auth}'`, (error, response) => {
-        if(error) {
-            console.log(error);
-            res.json([]);
-        }
-
-        else {
-            response.rows.forEach(row => favorites.push(row.recipelink));
-            res.json(favorites);
-        }
-    });
+    userRepository.getFavorites(req.body.auth).then(response => res.json(response));
 });
 
 routes.post('/favorite', (req, res) => {
-    db.query(`SELECT userEmail FROM USER_SESSION WHERE sessionId = '${req.body.auth}'`, (error, response) => {
-        if(error) {
-            console.log(error);
-            res.json({error: true});
-        }
-
-        else {
-            if(response.rows.length) {
-                db.query(`INSERT INTO FAVORITE VALUES ('${response.rows[0].useremail}', '${req.body.recipelink}')`, (error, response) => {
-                    if(error) {
-                        if(error.code == 23505) {
-                            db.query(`DELETE FROM FAVORITE WHERE recipelink = '${req.body.recipelink}'`, (error, response) => {
-                                if(error) {
-                                    console.log(error);
-                                    res.json({error: true});
-                                }
-
-                                else
-                                    res.json({error: false});
-                            })
-                        }
-
-                        else {
-                            console.log(error);
-                            res.json({error: true});
-                        }
-                    }
-
-                    else
-                        res.json({error: false});
-                });
-            }
-            else{
-                res.json({error: true});
-            }
-        }
-    });
+    userRepository.favorite(req.body.auth, req.body.recipelink).then(response => res.json(response));
 });
 
 module.exports = routes;
