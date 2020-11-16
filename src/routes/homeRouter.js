30790f54eb0e0e29e76778d952e767072605f515
Sam Buters <sambuters@gmail.com>
2020-11-16 16:50:25 -0300

    [#168] Adding routes information

--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -130,12 +130,144 @@ routes.get('/', async (req, res, next) => {
             		},
 
             		{
-            			name: 'recipelink',
+            			name: 'recipeLink',
+            			type: 'string',
+            			optional: false
+            		},
+					
+					{
+						name: 'recipeTitle',
+						type: 'string',
+						optional: false
+					},
+					
+					{
+						name: 'recipeImage',
+						type: 'string',
+						optional: false
+					}
+            	],
+            },
+			
+			createBook: {
+            	type: 'post',
+            	params: [
+            		{
+            			name: 'auth',
+            			type: 'string',
+            			optional: false
+            		},
+
+            		{
+            			name: 'title',
+            			type: 'string',
+            			optional: false
+            		},
+					
+					{
+            			name: 'description',
+            			type: 'string',
+            			optional: false
+            		},
+					
+					{
+            			name: 'visibility',
+            			type: 'boolean',
+            			optional: false
+            		}
+            	],
+            },
+			
+			deleteBook: {
+            	type: 'post',
+            	params: [
+            		{
+            			name: 'auth',
+            			type: 'string',
+            			optional: false
+            		},
+
+            		{
+            			name: 'bookId',
+            			type: 'int',
+            			optional: false
+            		}
+            	],
+            },
+			
+			getBooks: {
+            	type: 'post',
+            	params: [
+            		{
+            			name: 'auth',
             			type: 'string',
             			optional: false
             		}
             	],
-            }
+            },
+			
+			getBook: {
+            	type: 'post',
+            	params: [
+            		{
+            			name: 'auth',
+            			type: 'string',
+            			optional: false
+            		},
+
+            		{
+            			name: 'bookId',
+            			type: 'int',
+            			optional: false
+            		}
+            	],
+            },
+			
+			addBookRecipe: {
+            	type: 'post',
+            	params: [
+            		{
+            			name: 'auth',
+            			type: 'string',
+            			optional: false
+            		},
+
+            		{
+            			name: 'bookId',
+            			type: 'int',
+            			optional: false
+            		},
+					
+					{
+						name: 'recipeId',
+						type: 'int',
+						optional: false
+					}
+            	],
+            },
+			
+			deleteBookRecipe: {
+            	type: 'post',
+            	params: [
+            		{
+            			name: 'auth',
+            			type: 'string',
+            			optional: false
+            		},
+
+            		{
+            			name: 'bookId',
+            			type: 'int',
+            			optional: false
+            		},
+					
+					{
+						name: 'recipeId',
+						type: 'int',
+						optional: false
+					}
+            	],
+            },
         }]
     })
 });
