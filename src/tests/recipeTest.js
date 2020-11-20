9d28860a3b2712fd37eeab19213a7561051a70e8
Leticia Araujo <leticiards.04@gmail.com>
2020-11-20 00:38:11 -0300

    Correcting unnecessary documents after meeting.

--- src/tests/recipeTest.js
+++ src/tests/recipeTest.js
@@ -1,38 +1,17 @@
+const should = chai.should();
+
 const chai = require('chai');
-const Crawler = require('../models/Ingredient');
+const RecipesRepository = require('../Repository/RecipesRepository');
 
 const should = chai.should();
 
-describe('Ingredient', () => {
-  it('should get results', (done) => {
-	let test_recipes = new Ingredient('bolo');git 
-	
-	test_crawler.getResults().then((results) => {
-		results.should.be.a('Array').that.has.lengthOf(3);
-		
-		results.forEach((result) => {
-			result.should.have.property('name').to.be.oneOf(['TudoGostoso', 'Tastemade', 'TudoReceitas']);
-			
-			if(result.error) {
-				result.error.should.be.a('Number').that.is.oneOf([500, 503]);
-				result.should.have.property('results').that.is.an('Array').that.has.lengthOf(0);
-				
-				if(result.error == 500)
-					console.log(`\x1b[31mInternal Server Error: An error has occurred while handling ${result.name} information.\n\x1b[31mThis can be a result of the website changing their page structure.`);
-				
-				else
-					console.log(`\x1b[31mService Unavailable: An error has occurred while crawling ${result.name} information.\n\x1b[31mThis can be a connection problem or a bad URL.`);
-			}
-			
-			else {
-				result.should.have.property('results').that.is.an('Array');
-				result.results[0].should.have.property('link').that.is.a('String').that.includes('http');
-				result.results[0].should.have.property('title').that.is.a('String');
-				result.results[0].should.have.property('img_url').that.is.a('String').that.includes('http');
-			}
-		});
+describe('Recipe', () => {
+  it('add new recipe', (done) => {
 
-		done();
-	});
+    RecipesRepository.addRecipe("teste.teste@teste.com", "Bolo de Chocolate", 12, 50, true, "passos adicionados!").then((response) => {
+        response.should.be.a('Object').that.has.lengthOf(1);
+        response.should.have.property('error').to.be('false');
+    });
   }).timeout(10000);
 });
+
